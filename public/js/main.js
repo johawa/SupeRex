306db47b762d38ff33a52f90549bf08ab8fcf3d0
timebasher <johannes.walenta@googlemail.com>
2018-11-23 13:07:20 +0100

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+import SpriteSheet from "./SpriteSheet.js";
+import { loadImage, loadLevel } from "./Loaders.js";
+
+const canvas = document.getElementById("screen");
+const context = canvas.getContext("2d");
+
+context.fillRect(0, 0, 50, 50);
+
+loadImage("./img/tileset.png").then(image => {
+  const sprites = new SpriteSheet(image, 16, 16); //img, widht, height
+  sprites.define("ground", 0, 0);
+  sprites.define("sky", 3, 23);
+
+  loadLevel("1_1").then(level => {
+    console.log(level);
+  });
+
+  for (let x = 0; x < 25; ++x) {
+    for (let y = 0; y < 14; ++y) {
+      sprites.drawTile("sky", context, x, y);
+    }
+  }
+
+  for (let x = 0; x < 25; ++x) {
+    for (let y = 12; y < 14; ++y) {
+      sprites.drawTile("ground", context, x, y);
+    }
+  }
+});
